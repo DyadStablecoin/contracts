64fe8395fc8141d18cc2ec3aaa842d9250eeeeb8
shafu.eth <selfouly@gmail.com>
2023-12-02 14:32:52 +0100

    rm unused log

--- src/core/VaultManager.sol
+++ src/core/VaultManager.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import "forge-std/console.sol";
-
 import {DNft}          from "./DNft.sol";
 import {Dyad}          from "./Dyad.sol";
 import {Licenser}      from "./Licenser.sol";
