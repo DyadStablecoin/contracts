2bdd09a3007f0db1ec810c14a6cdec88853eb3db
shafu.eth <selfouly@gmail.com>
2023-12-06 18:55:23 +0100

    payments

---
+++ src/periphery/Payments.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import {VaultManager} from "../core/VaultManager.sol";
+import {Vault}        from "../core/Vault.sol";
+
+import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
+import {SafeTransferLib}   from "@solmate/src/utils/SafeTransferLib.sol";
+import {Owned}             from "@solmate/src/auth/Owned.sol";
+import {ERC20}             from "@solmate/src/tokens/ERC20.sol";
+
+contract Payments is Owned(msg.sender) {
+  using FixedPointMathLib for uint;
+  using SafeTransferLib   for ERC20;
+
+  VaultManager public immutable vaultManager;
+
+  uint    public fee;
+  address public feeRecipient;
+
+  constructor(VaultManager _vaultManager) { vaultManager = _vaultManager; }
+
+  function setFee(uint _fee) external onlyOwner { fee = _fee; }
+
+  function deposit(
+    uint    id,
+    address vault,
+    uint    amount
+  ) 
+    external 
+  {
+    uint feeAmount = amount.mulWadDown(fee);
+    Vault(vault).asset().safeTransferFrom(msg.sender, feeRecipient, feeAmount);
+    uint netAmount = amount - feeAmount;
+    vaultManager.deposit(id, vault, netAmount);
+  }
+}
