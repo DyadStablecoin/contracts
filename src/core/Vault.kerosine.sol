2dd926f10265555950293a84f506aa89bb07b521
shafu.eth <selfouly@gmail.com>
2024-02-08 20:28:39 +0100

    tvl

--- src/core/Vault.kerosine.sol
+++ src/core/Vault.kerosine.sol
@@ -3,16 +3,21 @@ pragma solidity =0.8.17;
 
 import {VaultManager}  from "./VaultManager.sol";
 import {Dyad}          from "./Dyad.sol";
+import {Vault}         from "./Vault.sol";
 import {IDNft}         from "../interfaces/IDNft.sol";
 import {IVault}        from "../interfaces/IVault.sol";
 
-import {SafeCast}          from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import {SafeTransferLib}   from "@solmate/src/utils/SafeTransferLib.sol";
-import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
-import {ERC20}             from "@solmate/src/tokens/ERC20.sol";
+import {EnumerableSet}   from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {SafeTransferLib} from "@solmate/src/utils/SafeTransferLib.sol";
+import {ERC20}           from "@solmate/src/tokens/ERC20.sol";
+import {Owned}           from "@solmate/src/auth/Owned.sol";
 
-contract Vault is IVault {
+contract KerosineVault is IVault, Owned(msg.sender) {
+  using EnumerableSet   for EnumerableSet.AddressSet;
   using SafeTransferLib for ERC20;
+
+  // TODO: add a limit to the number of vaults
+  EnumerableSet.AddressSet private vaults;
 
   VaultManager  public immutable vaultManager;
   ERC20         public immutable asset;
@@ -33,6 +38,24 @@ contract Vault is IVault {
     vaultManager   = _vaultManager;
     asset          = _asset;
     dyad           = _dyad;
+  }
+
+  function addVault(
+    address vault
+  ) 
+    external 
+      onlyOwner
+  {
+    vaults.add(vault);
+  }
+
+  function removeVault(
+    address vault
+  ) 
+    external 
+      onlyOwner
+  {
+    vaults.remove(vault);
   }
 
   function deposit(
@@ -85,7 +108,12 @@ contract Vault is IVault {
     public 
     view 
     returns (uint) {
-      // (TVL - DYAD supply) / Kerosine supply
-      return (0 - dyad.totalSupply()) / asset.totalSupply();
+      uint tvl;
+      uint numberOfVaults = vaults.length();
+      for (uint i = 0; i < numberOfVaults; i++) {
+        Vault vault = Vault(vaults.at(i));
+        tvl += vault.asset().balanceOf(address(vault)) * vault.assetPrice();
+      }
+      return (tvl - dyad.totalSupply()) / asset.totalSupply();
   }
 }
