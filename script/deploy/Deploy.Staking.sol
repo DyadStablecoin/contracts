2ba4bcbf25b66ecb2cc51ca4d4ccfce6685f5f3c
shafu.eth <selfouly@gmail.com>
2024-02-28 22:31:03 +0100

    correct params

--- script/deploy/Deploy.Staking.sol
+++ script/deploy/Deploy.Staking.sol
@@ -11,11 +11,23 @@ import {ERC20} from "@solmate/src/tokens/ERC20.sol";
 
 contract DeployStaking is Script, Parameters {
   function run() public {
+
+    uint ONE_MILLION = 1_000_000;
+
     vm.startBroadcast();  // ----------------------
 
     Kerosine kerosine = new Kerosine();
     Staking  staking  = new Staking(ERC20(MAINNET_WETH_DYAD_UNI), kerosine);
-    kerosine.transfer(address(staking), kerosine.totalSupply());
+
+    kerosine.transfer(
+      address(staking),
+      ONE_MILLION * 10**18 // 1 million
+    );
+
+    kerosine.transfer(
+      MAINNET_OWNER,                                // multi-sig
+      kerosine.totalSupply() - ONE_MILLION * 10**18 // the rest
+    );
 
     vm.stopBroadcast();  // ----------------------------
   }
