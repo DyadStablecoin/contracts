e705bee6795c2cfe4f46b00ade5b7af796668a1d
shafu.eth <selfouly@gmail.com>
2024-03-14 12:48:12 +0000

    deploy staking

---
+++ script/deploy/Deploy.Staking.Goerli.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+import {StakingDeployBase} from "./DeployBase.Staking.s.sol";
+import {Parameters} from "../../src/params/Parameters.sol";
+
+import {ERC20} from "@solmate/src/tokens/ERC20.sol";
+
+contract DeployStakingGoerli is Script, Parameters {
+  function run() public {
+    new StakingDeployBase().deploy(
+      GOERLI_OWNER,
+      1_000_000 * 10**18,
+      5 days,
+      ERC20(GOERLI_WETH_DYAD_UNI)
+    );
+  }
+}
+
