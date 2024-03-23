de925914465a9913d9460d3b5ca74bcfb5e3936c
shafu.eth <selfouly@gmail.com>
2024-03-23 13:18:18 +0200

    set denominator

--- src/staking/KerosineDenominator.sol
+++ src/staking/KerosineDenominator.sol
@@ -7,19 +7,16 @@ import {ERC20}                from "@solmate/src/tokens/ERC20.sol";
 contract KerosineDenominator {
 
   BoundedKerosineVault public boundedKerosineVault;
-  ERC20                public asset;
 
   constructor(
-    BoundedKerosineVault _boundedKerosineVault,
-    ERC20                _asset
+    BoundedKerosineVault _boundedKerosineVault
   ) {
     boundedKerosineVault = _boundedKerosineVault;
-    asset                = _asset;
   }
 
   function denominator() external view returns (uint) {
     uint boundedKerosine = boundedKerosineVault.deposits();
-    return asset.totalSupply() + 2*boundedKerosine;
+    return boundedKerosineVault.asset().totalSupply() + 2*boundedKerosine;
   } 
 
 }
