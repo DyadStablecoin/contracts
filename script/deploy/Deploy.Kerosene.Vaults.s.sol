f9ec79d476aaac38dcfd6fef19e86869f13c0c47
shafu.eth <selfouly@gmail.com>
2024-03-27 16:48:33 +0200

    script

---
+++ script/deploy/Deploy.Kerosene.Vaults.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+
+import {Parameters} from "../../src/params/Parameters.sol";
+
+contract DeployKeroseneVaults is Script, Parameters {
+
+}
