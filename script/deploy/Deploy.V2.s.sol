feee09f390e3a2b29991ae2d13e30c0e87b7893d
shafu.eth <selfouly@gmail.com>
2024-04-10 01:49:39 +0200

    v2 deploy script

--- script/deploy/Deploy.V2.s.sol
+++ script/deploy/Deploy.V2.s.sol
@@ -3,16 +3,20 @@ pragma solidity =0.8.17;
 
 import "forge-std/Script.sol";
 
-import {Parameters}   from "../../src/params/Parameters.sol";
-import {VaultManager} from "../../src/core/VaultManager.sol";
-import {DNft}         from "../../src/core/DNft.sol";
-import {Dyad}         from "../../src/core/Dyad.sol";
-import {Licenser}     from "../../src/core/Licenser.sol";
-import {Vault}        from "../../src/core/Vault.sol";
-import {VaultWstEth}  from "../../src/core/Vault.wsteth.sol";
-import {Payments}     from "../../src/periphery/Payments.sol";
-import {IWETH}        from "../../src/interfaces/IWETH.sol";
-import {IAggregatorV3} from "../../src/interfaces/IAggregatorV3.sol";
+import {Parameters}             from "../../src/params/Parameters.sol";
+import {VaultManagerV2}         from "../../src/core/VaultManagerV2.sol";
+import {DNft}                   from "../../src/core/DNft.sol";
+import {Dyad}                   from "../../src/core/Dyad.sol";
+import {Licenser}               from "../../src/core/Licenser.sol";
+import {Vault}                  from "../../src/core/Vault.sol";
+import {VaultWstEth}            from "../../src/core/Vault.wsteth.sol";
+import {IWETH}                  from "../../src/interfaces/IWETH.sol";
+import {IAggregatorV3}          from "../../src/interfaces/IAggregatorV3.sol";
+import {KerosineManager}        from "../../src/core/KerosineManager.sol";
+import {UnboundedKerosineVault} from "../../src/core/Vault.kerosine.unbounded.sol";
+import {BoundedKerosineVault}   from "../../src/core/Vault.kerosine.bounded.sol";
+import {Kerosine}               from "../../src/staking/Kerosine.sol";
+import {KerosineDenominator}    from "../../src/staking/KerosineDenominator.sol";
 
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
 
@@ -22,36 +26,60 @@ contract DeployV2 is Script, Parameters {
 
     Licenser vaultLicenser = new Licenser();
 
-    // Vault Manager needs to be licensed
-    VaultManager vaultManager = new VaultManager(
+    // Vault Manager needs to be licensed through the Vault Manager Licenser
+    VaultManagerV2 vaultManager = new VaultManagerV2(
       DNft(MAINNET_DNFT),
       Dyad(MAINNET_DYAD),
-      Licenser(vaultLicenser)
+      vaultLicenser
     );
 
     // weth vault
-    Vault vault = new Vault(
+    Vault ethVault = new Vault(
       vaultManager,
-      ERC20(MAINNET_WETH),
+      ERC20        (MAINNET_WETH),
       IAggregatorV3(MAINNET_WETH_ORACLE)
     );
 
     // wsteth vault
-    new VaultWstEth(
+    VaultWstEth wstEth = new VaultWstEth(
       vaultManager, 
       ERC20        (MAINNET_WSTETH), 
       IAggregatorV3(MAINNET_CHAINLINK_STETH)
     );
 
-    // vaults need to be licensed!
+    KerosineManager kerosineManager = new KerosineManager();
 
-    Payments payments = new Payments(
+    kerosineManager.add(address(ethVault));
+    kerosineManager.add(address(wstEth));
+
+    kerosineManager.transferOwnership(MAINNET_OWNER);
+
+    UnboundedKerosineVault unboundedKerosineVault = new UnboundedKerosineVault(
       vaultManager,
-      IWETH(MAINNET_WETH)
+      Kerosine(MAINNET_KEROSENE), 
+      Dyad    (MAINNET_DYAD),
+      kerosineManager
     );
 
-    payments.setFee(MAINNET_FEE);
-    payments.setFeeRecipient(MAINNET_FEE_RECIPIENT);
+    BoundedKerosineVault boundedKerosineVault     = new BoundedKerosineVault(
+      vaultManager,
+      Kerosine(MAINNET_KEROSENE), 
+      Dyad    (MAINNET_DYAD),
+      kerosineManager
+    );
+
+    KerosineDenominator kerosineDenominator       = new KerosineDenominator(
+      boundedKerosineVault
+    );
+
+    unboundedKerosineVault.setDenominator(kerosineDenominator);
+
+    vaultLicenser.add(address(ethVault));
+    vaultLicenser.add(address(wstEth));
+    vaultLicenser.add(address(unboundedKerosineVault));
+    vaultLicenser.add(address(boundedKerosineVault));
+
+    vaultLicenser.transferOwnership(MAINNET_OWNER);
 
     vm.stopBroadcast();  // ----------------------------
   }
