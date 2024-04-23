83aff47d8c362251d5ec8cfb4ec8d6c478398204
shafu.eth <selfouly@gmail.com>
2024-04-23 22:58:33 +0200

    refactor

--- src/core/KeroseneOracle.sol
+++ src/core/KeroseneOracle.sol
@@ -5,8 +5,38 @@ import {IAggregatorV3} from "../interfaces/IAggregatorV3.sol";
 
 contract KeroseneOracle is IAggregatorV3 {
   function decimals() external pure returns (uint8) { return 18; }
-  function description() external pure override returns (string memory) { return "Kerosene Oracle"; }
-  function version() external pure override returns (uint256) { return 1; }
-  function getRoundData(uint80 _roundId) external pure override returns (uint80, int256, uint256, uint256, uint80) { return (_roundId, 0, 0, 0, 0); }
-  function latestRoundData() external pure override returns (uint80, int256, uint256, uint256, uint80) { return (0, 0, 0, 0, 0); }
+
+  function description() external pure override returns (string memory) {
+    return "Kerosene Oracle"; 
+  }
+
+  function version() external pure override returns (uint256) {
+    return 1;
+  }
+
+  function getRoundData(uint80 _roundId) 
+    external
+    pure
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    ) {
+      return (_roundId, 0, 0, 0, 0); 
+  }
+
+  function latestRoundData() 
+    external
+    pure
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    ) {
+      return (0, 0, 0, 0, 0); 
+  }
 }
