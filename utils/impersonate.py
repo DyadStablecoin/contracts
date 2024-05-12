6a7b1ab9f12e9a0bfb8fa39c91fc3c80eeaa6c59
shafu.eth <selfouly@gmail.com>
2024-05-12 23:44:47 +0200

    anvil_setBalance

--- utils/impersonate.py
+++ utils/impersonate.py
@@ -12,21 +12,7 @@ ANVIL_PRIVATE_KEY = "0x7c852118294e51e653712a81e05800f419141751be58f605c371e1514
 if __name__ == "__main__":
   web3 = Web3(Web3.HTTPProvider(RPC))
 
-  # We need to give the token holder some ETH so he can pay for the transfer
-  amount_in_wei = web3.toWei(1, 'ether') 
-
-  transaction = {
-      'to':       TOKEN_HOLDER,
-      'value':    amount_in_wei,
-      'gas':      21000,  
-      'gasPrice': web3.toWei('50', 'gwei'),  # Gas price (in Gwei)
-      'nonce':    web3.eth.get_transaction_count(ANVIL_PUBLIC_KEY),
-      'chainId':  web3.eth.chain_id
-  }
-
-  signed_tx = web3.eth.account.sign_transaction(transaction, ANVIL_PRIVATE_KEY)
-  tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-  print("Sending ETH to:", TOKEN_HOLDER)
+  web3.provider.make_request("anvil_setBalance", [TOKEN_HOLDER, web3.toWei(1, 'ether')])  
 
   balance = web3.eth.get_balance(TOKEN_HOLDER)
   assert balance > 0
@@ -37,3 +23,5 @@ if __name__ == "__main__":
 
   # calls the forge script that will transfer the tokens from the token holder
   subprocess.run(["make", f"TOKEN_HOLDER={TOKEN_HOLDER}", "transfer"])
+
+  web3.provider.make_request("anvil_stopImpersonatingAccount", []) 
