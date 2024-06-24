8f131b12440e59d66bb2f59e40dfa072741c9b7d
shafu.eth <selfouly@gmail.com>
2024-06-24 11:16:06 +0300

    return early

--- src/core/VaultManagerV3.sol
+++ src/core/VaultManagerV3.sol
@@ -185,6 +185,7 @@ contract VaultManagerV3 is IVaultManager, UUPSUpgradeable, OwnableUpgradeable {
       lastDeposit[to] = block.number; // `move` acts like a deposit
 
       uint totalValue  = getTotalValue(id);
+      if (totalValue == 0) return;
       uint reward_rate = amount
                           .divWadDown(debt)
                           .mulWadDown(LIQUIDATION_REWARD);
