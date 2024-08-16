b5d9a5cd15220165666e54ae34104512965e9285
Coffee☕️ <coffee@coffeebot.dev>
2024-08-16 11:32:25 -0400

    add fuzz test

--- test/Vault.weeth.t.sol
+++ test/Vault.weeth.t.sol
@@ -12,21 +12,47 @@ import {IVault} from "../src/interfaces/IVault.sol";
 import {DNft} from "../src/core/DNft.sol";
 
 contract VaultWeETHTest is Test, Parameters {
-  VaultWeETH vault;
+    VaultWeETH vault;
 
-  function setUp() public {
-    vault = new VaultWeETH(
-      MAINNET_FEE_RECIPIENT,
-      VaultManager (MAINNET_VAULT_MANAGER), 
-      ERC20        (MAINNET_WEETH), 
-      IAggregatorV3(MAINNET_CHAINLINK_WEETH),
-      IVault(MAINNET_V2_WETH_VAULT),
-      DNft(MAINNET_DNFT)
-    );
-  }
+    uint256 depositCap = 100 ether;
 
-  function test_assetPrice() public {
-    uint256 price = vault.assetPrice();
-    console.log("price: %s", price);
-  }
+    function setUp() public {
+        vault = new VaultWeETH(
+            MAINNET_FEE_RECIPIENT,
+            VaultManager(MAINNET_V2_VAULT_MANAGER),
+            // use WETH for test so we can simulate deposit
+            ERC20(MAINNET_WEETH),
+            IAggregatorV3(MAINNET_CHAINLINK_WEETH),
+            IVault(MAINNET_V2_WETH_VAULT),
+            DNft(MAINNET_DNFT)
+        );
+
+        vm.prank(MAINNET_FEE_RECIPIENT);
+        vault.setDepositCap(depositCap);
+    }
+
+    function test_assetPrice() public {
+        uint256 price = vault.assetPrice();
+        console.log("price: %s", price);
+    }
+
+    function testFuzzDepositCap(
+        uint256 currentDeposit,
+        uint256 depositAmount
+    ) public {
+        vm.assume(depositAmount < type(uint128).max);
+        vm.assume(currentDeposit < depositCap);
+
+        vm.mockCall(
+            MAINNET_WEETH,
+            abi.encodeWithSignature("balanceOf(address)", address(vault)),
+            abi.encode(currentDeposit)
+        );
+
+        vm.prank(MAINNET_V2_VAULT_MANAGER);
+        if (currentDeposit + depositAmount > depositCap) {
+            vm.expectRevert(VaultWeETH.ExceedsDepositCap.selector);
+        }
+        vault.deposit(1, depositAmount);
+    }
 }
