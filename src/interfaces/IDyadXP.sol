1374d4dd188ab215bc8f274f046cfc20e09309bb
shafu <selfouly@gmail.com>
2024-09-20 18:35:34 +0200

    update vaultMAnagerv5

--- src/interfaces/IDyadXP.sol
+++ src/interfaces/IDyadXP.sol
@@ -4,5 +4,8 @@ pragma solidity ^0.8.17;
 interface IDyadXP {
   function updateXP(uint256 noteId) external;
   function beforeKeroseneWithdrawn(uint256 noteId, uint256 amountWithdrawn) external;
+  function beforeKeroseneDeposited(uint256 noteId, uint256 amountDeposited) external;
+  function afterDyadMinted(uint256 noteId) external;
+  function afterDyadBurned(uint256 noteId) external;
   function balanceOfNote(uint256 noteId) external view returns (uint256);
 }
