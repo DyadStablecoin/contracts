d8e418887640e7035e41e24d7e40a1af4f3edd4e
shafu <selfouly@gmail.com>
2024-09-20 17:00:11 +0200

    check for ticks

--- src/staking/UniswapV3Staking.sol
+++ src/staking/UniswapV3Staking.sol
@@ -1,24 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../interfaces/IDyadXP.sol"; 
+import "../interfaces/IDyadXP.sol";
 import "../interfaces/INonfungiblePositionManager.sol";
 import {DNft} from "../core/DNft.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {UUPSUpgradeable}    from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable { 
+contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
     IERC20 public rewardsToken;
     INonfungiblePositionManager public positionManager;
-    IDyadXP public dyadXP; 
+    IDyadXP public dyadXP;
     DNft public dnft;
-    uint256 public rewardsRate; 
+    uint256 public rewardsRate;
     address public rewardsTokenHolder;
     address public token0;
     address public token1;
     uint24 public poolFee;
+    int24 public minTick;
+    int24 public maxTick;
 
     struct StakeInfo {
         uint256 liquidity;
@@ -27,23 +29,25 @@ contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
         bool isStaked;
     }
 
-    mapping(uint256 => StakeInfo) public stakes; 
+    mapping(uint256 => StakeInfo) public stakes;
 
     event Staked(address indexed user, uint256 noteId, uint256 tokenId, uint256 liquidity);
     event Unstaked(address indexed user, uint256 noteId, uint256 tokenId);
     event RewardClaimed(address indexed user, uint256 reward);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() { _disableInitializers(); }
+    constructor() {
+        _disableInitializers();
+    }
 
     function initialize(
-        address _owner, 
+        address _owner,
         IERC20 _rewardsToken,
         INonfungiblePositionManager _positionManager,
         IDyadXP _dyadXP,
-        DNft _dnft, 
+        DNft _dnft,
         uint256 _rewardsRate,
-        address _rewardsTokenHolder, 
+        address _rewardsTokenHolder,
         address _token0,
         address _token1,
         uint24 _poolFee
@@ -53,7 +57,7 @@ contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
 
         rewardsToken = _rewardsToken;
         positionManager = _positionManager;
-        dyadXP = _dyadXP; 
+        dyadXP = _dyadXP;
         dnft = _dnft;
         rewardsRate = _rewardsRate;
         rewardsTokenHolder = _rewardsTokenHolder;
@@ -66,38 +70,35 @@ contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
         require(dnft.ownerOf(noteId) == msg.sender, "You are not the Note owner");
 
         StakeInfo storage stakeInfo = stakes[noteId];
-        require(!stakeInfo.isStaked, "Note already used for staking"); 
+        require(!stakeInfo.isStaked, "Note already used for staking");
 
         (
-          , 
-          , 
-          address positionToken0, 
-          address positionToken1, 
-          uint24 positionFee, 
-          , 
-          , 
-          uint128 liquidity, 
-          , 
-          ,
-          , 
+            ,
+            ,
+            address positionToken0,
+            address positionToken1,
+            uint24 positionFee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            ,
+            ,
+            ,
         ) = positionManager.positions(tokenId);
 
         require(liquidity > 0, "No liquidity");
         require(
-          (positionToken0 == token0 && positionToken1 == token1) ||
-          (positionToken0 == token1 && positionToken1 == token0),
-          "Invalid token pair"
+            (positionToken0 == token0 && positionToken1 == token1)
+                || (positionToken0 == token1 && positionToken1 == token0),
+            "Invalid token pair"
         );
         require(positionFee == poolFee, "Invalid fee");
+        require(tickLower >= minTick && tickUpper <= maxTick, "Position tick range out of bounds");
 
         positionManager.safeTransferFrom(msg.sender, address(this), tokenId);
 
-        stakes[noteId] = StakeInfo({
-          liquidity: liquidity,
-          lastRewardTime: block.timestamp,
-          tokenId: tokenId,
-          isStaked: true
-        });
+        stakes[noteId] =
+            StakeInfo({liquidity: liquidity, lastRewardTime: block.timestamp, tokenId: tokenId, isStaked: true});
 
         emit Staked(msg.sender, noteId, tokenId, liquidity);
     }
@@ -135,10 +136,10 @@ contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
     function _calculateRewards(uint256 noteId, StakeInfo storage stakeInfo) internal view returns (uint256) {
         uint256 timeDiff = block.timestamp - stakeInfo.lastRewardTime;
 
-        uint256 xp = dyadXP.balanceOfNote(noteId); 
+        uint256 xp = dyadXP.balanceOfNote(noteId);
 
-       (,,,,,,, uint128 liquidity,,,,) = positionManager.positions(stakeInfo.tokenId);  
-       return timeDiff * rewardsRate * liquidity / 1e18 * xp / 1e18;
+        (,,,,,,, uint128 liquidity,,,,) = positionManager.positions(stakeInfo.tokenId);
+        return timeDiff * rewardsRate * liquidity / 1e18 * xp / 1e18;
     }
 
     function currentRewards(uint256 noteId) external view returns (uint256) {
@@ -146,23 +147,29 @@ contract UniswapV3Staking is UUPSUpgradeable, OwnableUpgradeable {
         return _calculateRewards(noteId, stakeInfo);
     }
 
-    function setRewardsRate(uint256 _rewardsRate) external onlyOwner { 
-        rewardsRate = _rewardsRate; 
+    function setRewardsRate(uint256 _rewardsRate) external onlyOwner {
+        rewardsRate = _rewardsRate;
     }
 
-    function setRewardsTokenHolder(address _rewardsTokenHolder) external onlyOwner { 
-        rewardsTokenHolder = _rewardsTokenHolder; 
+    function setRewardsTokenHolder(address _rewardsTokenHolder) external onlyOwner {
+        rewardsTokenHolder = _rewardsTokenHolder;
     }
 
     function setPoolParameters(address _token0, address _token1, uint24 _poolFee) external onlyOwner {
-      token0 = _token0;
-      token1 = _token1;
-      poolFee = _poolFee;
+        token0 = _token0;
+        token1 = _token1;
+        poolFee = _poolFee;
+    }
+
+    function setTickRange(int24 _minTick, int24 _maxTick) external onlyOwner {
+        require(_minTick <= _maxTick, "Invalid tick range");
+        minTick = _minTick;
+        maxTick = _maxTick;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
-      return this.onERC721Received.selector;
+        return this.onERC721Received.selector;
     }
 }
