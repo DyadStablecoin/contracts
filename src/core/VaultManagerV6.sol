cb27b3bccf9c204fdc874b534e06259385acb110
Coffee☕️ <coffee@coffeebot.dev>
2024-11-02 15:35:33 -0400

    gas optimize and consolidate data loading

---
+++ src/core/VaultManagerV6.sol
@@ -0,0 +1,370 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {DNft} from "./DNft.sol";
+import {Dyad} from "./Dyad.sol";
+import {VaultLicenser} from "./VaultLicenser.sol";
+import {Vault} from "./Vault.sol";
+import {DyadXP} from "../staking/DyadXP.sol";
+import {IVaultManagerV5} from "../interfaces/IVaultManagerV5.sol";
+import {DyadHooks} from "./DyadHooks.sol";
+import "../interfaces/IExtension.sol";
+
+import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
+import {ERC20} from "@solmate/src/tokens/ERC20.sol";
+import {SafeTransferLib} from "@solmate/src/utils/SafeTransferLib.sol";
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/// @custom:oz-upgrades-from src/core/VaultManagerV4.sol:VaultManagerV4
+contract VaultManagerV6 is IVaultManagerV5, UUPSUpgradeable, OwnableUpgradeable {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using FixedPointMathLib for uint256;
+    using SafeTransferLib for ERC20;
+
+    uint256 public constant MAX_VAULTS = 6;
+    uint256 public constant MIN_COLLAT_RATIO = 1.5e18; // 150% // Collaterization
+    uint256 public constant LIQUIDATION_REWARD = 0.2e18; //  20%
+
+    address public constant KEROSENE_VAULT = 0x4808e4CC6a2Ba764778A0351E1Be198494aF0b43;
+
+    DNft public dNft;
+    Dyad public dyad;
+    VaultLicenser public vaultLicenser;
+
+    mapping(uint256 id => EnumerableSet.AddressSet vaults) internal vaults;
+    mapping(uint256 id => uint256 block) private lastDeposit;
+
+    DyadXP public dyadXP;
+
+    /// @notice Extensions authorized for use in the system, with bitmap of enabled hooks
+    mapping(address => uint256) private _systemExtensions;
+
+    /// @notice Extensions authorized by a user for use on their notes
+    mapping(address user => EnumerableSet.AddressSet) private _authorizedExtensions;
+
+    modifier isValidDNft(uint256 id) {
+        if (dNft.ownerOf(id) == address(0)) revert InvalidDNft();
+        _;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public reinitializer(6) {
+        // Nothing to initialize right now
+    }
+
+    /// @inheritdoc IVaultManagerV5
+    function add(uint256 id, address vault) external {
+        _authorizeCall(id);
+        if (!vaultLicenser.isLicensed(vault)) revert VaultNotLicensed();
+        if (vaults[id].length() >= MAX_VAULTS) revert TooManyVaults();
+        if (vaults[id].add(vault)) {
+            emit Added(id, vault);
+        }
+    }
+
+    /// @inheritdoc IVaultManagerV5
+    function remove(uint256 id, address vault) external {
+        _authorizeCall(id);
+        if (vaults[id].remove(vault)) {
+            if (Vault(vault).id2asset(id) > 0) {
+                if (vaultLicenser.isLicensed(vault)) {
+                    _checkExoValueAndCollatRatio(id);
+                }
+            }
+            emit Removed(id, vault);
+        }
+    }
+
+    /// @inheritdoc IVaultManagerV5
+    function deposit(uint256 id, address vault, uint256 amount) external isValidDNft(id) {
+        _authorizeCall(id);
+        lastDeposit[id] = block.number;
+        Vault _vault = Vault(vault);
+        _vault.asset().safeTransferFrom(msg.sender, vault, amount);
+        _vault.deposit(id, amount);
+
+        if (vault == KEROSENE_VAULT) {
+            dyadXP.afterKeroseneDeposited(id, amount);
+        }
+    }
+
+    /// @inheritdoc IVaultManagerV5
+    function withdraw(uint256 id, address vault, uint256 amount, address to) public {
+        uint256 extensionFlags = _authorizeCall(id);
+        if (lastDeposit[id] == block.number) revert CanNotWithdrawInSameBlock();
+        if (vault == KEROSENE_VAULT) dyadXP.beforeKeroseneWithdrawn(id, amount);
+        Vault(vault).withdraw(id, to, amount); // changes `exo` or `kero` value and `cr`
+        if (DyadHooks.hookEnabled(extensionFlags, DyadHooks.AFTER_WITHDRAW)) {
+            IAfterWithdrawHook(msg.sender).afterWithdraw(id, vault, amount, to);
+        }
+        _checkExoValueAndCollatRatio(id);
+    }
+
+    //// @inheritdoc IVaultManagerV5
+    function mintDyad(uint256 id, uint256 amount, address to) external {
+        uint256 extensionFlags = _authorizeCall(id);
+        dyad.mint(id, to, amount); // changes `mintedDyad` and `cr`
+        if (DyadHooks.hookEnabled(extensionFlags, DyadHooks.AFTER_MINT)) {
+            IAfterMintHook(msg.sender).afterMint(id, amount, to);
+        }
+        _checkExoValueAndCollatRatio(id);
+        emit MintDyad(id, amount, to);
+    }
+
+    /// @notice Checks the exogenous collateral value and collateral ratio for the specified note.
+    /// @dev Reverts if the exogenous collateral value is less than the minted dyad or the collateral
+    ///      ratio is below the minimum.
+    function _checkExoValueAndCollatRatio(uint256 id) internal view {
+        (, uint256 exoValue,, uint256 cr, uint256 mintedDyad) = _totalVaultValuesAndCr(id);
+        if (exoValue < mintedDyad) {
+            revert NotEnoughExoCollat();
+        }
+        if (cr < MIN_COLLAT_RATIO) {
+            revert CrTooLow();
+        }
+    }
+
+    /// @inheritdoc IVaultManagerV5
+    function burnDyad(uint256 id, uint256 amount) public isValidDNft(id) {
+        dyad.burn(id, msg.sender, amount);
+        emit BurnDyad(id, amount, msg.sender);
+    }
+
+    /// @notice Liquidates the specified note, transferring collateral to the specified note.
+    /// @param id The note id
+    /// @param to The address to transfer the collateral to
+    /// @param amount The amount of dyad to liquidate
+    function liquidate(uint256 id, uint256 to, uint256 amount)
+        external
+        isValidDNft(id)
+        isValidDNft(to)
+        returns (address[] memory, uint256[] memory)
+    {
+        (uint256[] memory vaultsValues, uint256 exoValue, uint256 keroValue, uint256 cr, uint256 debt) =
+            _totalVaultValuesAndCr(id);
+
+        if (cr >= MIN_COLLAT_RATIO) revert CrTooHigh();
+        dyad.burn(id, msg.sender, amount); // changes `debt` and `cr`
+
+        lastDeposit[to] = block.number; // `move` acts like a deposit
+
+        uint256 numberOfVaults = vaults[id].length();
+        address[] memory vaultAddresses = new address[](numberOfVaults);
+        uint256[] memory vaultAmounts = new uint256[](numberOfVaults);
+
+        uint256 totalValue = exoValue + keroValue;
+        if (totalValue == 0) return (vaultAddresses, vaultAmounts);
+
+        for (uint256 i = 0; i < numberOfVaults; i++) {
+            Vault vault = Vault(vaults[id].at(i));
+            vaultAddresses[i] = address(vault);
+            if (vaultLicenser.isLicensed(address(vault))) {
+                uint256 depositAmount = vault.id2asset(id);
+                if (depositAmount == 0) continue;
+                uint256 value = vaultsValues[i];
+                if (value == 0) continue;
+                uint256 asset;
+                if (cr < LIQUIDATION_REWARD + 1e18 && debt != amount) {
+                    uint256 cappedCr = cr < 1e18 ? 1e18 : cr;
+                    uint256 liquidationEquityShare = (cappedCr - 1e18).mulWadDown(LIQUIDATION_REWARD);
+                    uint256 liquidationAssetShare = (liquidationEquityShare + 1e18).divWadDown(cappedCr);
+                    uint256 allAsset = depositAmount.mulWadUp(liquidationAssetShare);
+                    asset = allAsset.mulWadDown(amount).divWadDown(debt);
+                } else {
+                    uint256 share = value.divWadDown(totalValue);
+                    uint256 amountShare = share.mulWadUp(amount);
+                    uint256 reward_rate = amount.divWadDown(debt).mulWadDown(LIQUIDATION_REWARD);
+                    uint256 valueToMove = amountShare + amountShare.mulWadUp(reward_rate);
+                    uint256 cappedValue = valueToMove > value ? value : valueToMove;
+                    asset = cappedValue * (10 ** (vault.oracle().decimals() + vault.asset().decimals()))
+                        / vault.assetPrice() / 1e18;
+                }
+                vaultAmounts[i] = asset;
+                if (address(vault) == KEROSENE_VAULT) {
+                    dyadXP.beforeKeroseneWithdrawn(id, asset);
+                }
+                vault.move(id, to, asset);
+                if (address(vault) == KEROSENE_VAULT) {
+                    dyadXP.afterKeroseneDeposited(to, asset);
+                }
+            }
+        }
+
+        emit Liquidate(id, msg.sender, to, amount);
+
+        return (vaultAddresses, vaultAmounts);
+    }
+
+    /// @notice Returns the collateral ratio for the specified note.
+    /// @param id The note id
+    function collatRatio(uint256 id) external view returns (uint256) {
+        (,,, uint256 cr,) = _totalVaultValuesAndCr(id);
+        return cr;
+    }
+
+    /// @dev Internal function for computing collateral ratio. Reading `mintedDyad` and `totalValue`
+    ///      is expensive. If we already have these values loaded, we can re-use the cached values.
+    /// @param mintedDyad The amount of dyad minted for the note
+    /// @param totalValue The total value of all exogenous collateral for the note
+    function _collatRatio(
+        uint256 mintedDyad,
+        uint256 totalValue // in USD
+    ) internal pure returns (uint256) {
+        if (mintedDyad == 0) return type(uint256).max;
+        return totalValue.divWadDown(mintedDyad);
+    }
+
+    /// @notice Returns the total value of all exogenous and kerosene collateral for the specified note.
+    /// @param id The note id
+    function getTotalValue( // in USD
+    uint256 id)
+        external
+        view
+        returns (uint256)
+    {
+        (, uint256 exoValue, uint256 keroValue,,) = _totalVaultValuesAndCr(id);
+        return exoValue + keroValue;
+    }
+
+    /// @notice Returns the USD value of all exogenous and kerosene collateral for the specified note.
+    /// @param id The note id
+    function getVaultsValues( // in USD
+    uint256 id)
+        external
+        view
+        returns (
+            uint256 exoValue, // exo := exogenous (non-kerosene)
+            uint256 keroValue
+        )
+    {
+        (, exoValue, keroValue,,) = _totalVaultValuesAndCr(id);
+        return (exoValue, keroValue);
+    }
+
+    // ----------------- MISC ----------------- //
+    /// @notice Returns the registered vaults for the specified note
+    /// @param id The note id
+    function getVaults(uint256 id) external view returns (address[] memory) {
+        return vaults[id].values();
+    }
+
+    /// @notice Returns whether the specified vault is registered for the specified note
+    /// @param id The note id
+    /// @param vault The vault address
+    function hasVault(uint256 id, address vault) external view returns (bool) {
+        return vaults[id].contains(vault);
+    }
+
+    /// @notice Authorizes an extension for use by current user
+    /// @dev Can not authorize an extension that is not a registered and enabled system extension,
+    ///      but can deauthorize it
+    /// @param extension The extension address
+    /// @param isAuthorized Whether the extension is authorized
+    function authorizeExtension(address extension, bool isAuthorized) external {
+        bool authorizationChanged = false;
+        if (isAuthorized) {
+            if (!DyadHooks.hookEnabled(_systemExtensions[extension], DyadHooks.EXTENSION_ENABLED)) {
+                revert Unauthorized();
+            }
+            authorizationChanged = _authorizedExtensions[msg.sender].add(extension);
+        } else {
+            authorizationChanged = _authorizedExtensions[msg.sender].remove(extension);
+        }
+        if (authorizationChanged) {
+            emit UserExtensionAuthorized(msg.sender, extension, isAuthorized);
+        }
+    }
+
+    /// @notice Authorizes an extension for use in the system
+    /// @param extension The extension address
+    /// @param isAuthorized Whether the extension is authorized
+    function authorizeSystemExtension(address extension, bool isAuthorized) external onlyOwner {
+        uint256 hooks;
+        if (isAuthorized) {
+            hooks = DyadHooks.enableExtension(IExtension(extension).getHookFlags());
+        } else {
+            hooks = DyadHooks.disableExtension(_systemExtensions[extension]);
+        }
+        _systemExtensions[extension] = hooks;
+        emit SystemExtensionAuthorized(extension, hooks);
+    }
+
+    /// @notice Returns whether the specified extension is authorized for use in the system
+    /// @param extension The extension address
+    function isSystemExtension(address extension) external view returns (bool) {
+        return DyadHooks.hookEnabled(_systemExtensions[extension], DyadHooks.EXTENSION_ENABLED);
+    }
+
+    /// @notice Returns the authorized extensions for the specified user
+    /// @param user The user address
+    function authorizedExtensions(address user) external view returns (address[] memory) {
+        return _authorizedExtensions[user].values();
+    }
+
+    /// @notice Returns whether the specified extension is authorized for use by the specified user
+    /// @param user The user address
+    /// @param extension The extension address
+    function isExtensionAuthorized(address user, address extension) public view returns (bool) {
+        return _authorizedExtensions[user].contains(extension);
+    }
+
+    function _totalVaultValuesAndCr(uint256 id)
+        private
+        view
+        returns (uint256[] memory vaultValues, uint256 exoValue, uint256 keroValue, uint256 cr, uint256 mintedDyad)
+    {
+        uint256 numberOfVaults = vaults[id].length();
+        vaultValues = new uint256[](numberOfVaults);
+
+        for (uint256 i = 0; i < numberOfVaults; i++) {
+            Vault vault = Vault(vaults[id].at(i));
+            if (vaultLicenser.isLicensed(address(vault))) {
+                uint256 value = vault.getUsdValue(id);
+                vaultValues[i] = value;
+                if (vaultLicenser.isKerosene(address(vault))) {
+                    keroValue += value;
+                } else {
+                    exoValue += value;
+                }
+            }
+        }
+
+        mintedDyad = dyad.mintedDyad(id);
+        uint256 totalValue = exoValue + keroValue;
+        cr = _collatRatio(mintedDyad, totalValue);
+
+        return (vaultValues, exoValue, keroValue, cr, mintedDyad);
+    }
+
+    // ----------------- UPGRADABILITY ----------------- //
+    /// @dev UUPS upgrade authorization - only owner can upgrade
+    function _authorizeUpgrade(address) internal view override onlyOwner {}
+
+    /// @dev Authorizes that the caller is either the owner of the specified note, or a system extension
+    ///      that is both enabled and authorized by the owner of the note. Returns the extension flags if
+    ///      the caller is an authorized extension. Reverts if the caller is not authorized.
+    /// @param id The note id
+    function _authorizeCall(uint256 id) internal view returns (uint256) {
+        address dnftOwner = dNft.ownerOf(id);
+        if (dnftOwner != msg.sender) {
+            uint256 extensionFlags = _systemExtensions[msg.sender];
+            if (!DyadHooks.hookEnabled(extensionFlags, DyadHooks.EXTENSION_ENABLED)) {
+                revert Unauthorized();
+            }
+            if (!_authorizedExtensions[dnftOwner].contains(msg.sender)) {
+                revert Unauthorized();
+            }
+            return extensionFlags;
+        }
+        return 0;
+    }
+}
---
+++ test/fork/v6/v6.Liquidate.t.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import {BaseTestV6} from "./BaseV6.sol";
+import {VaultManagerV3} from "../../../src/core/VaultManagerV3.sol";
+import {Parameters} from "../../../src/params/Parameters.sol";
+import {DeployVaultManagerV3} from "../../../script/deploy/DeployVaultManagerV3.s.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+contract LiquidateTest is BaseTestV6 {
+    modifier mintDyad(uint256 id, uint256 amount) {
+        vm.prank(contracts.dNft.ownerOf(id));
+        contracts.vaultManager.mintDyad(id, amount, address(this));
+        _;
+    }
+
+    modifier liquidate(uint256 id, uint256 to, address liquidator) {
+        deal(address(contracts.dyad), liquidator, _ethToUSD(getMintedDyad(id)));
+        vm.prank(liquidator);
+        contracts.vaultManager.liquidate(id, to, getMintedDyad(id));
+        _;
+    }
+
+    function test_LiquidateWithManyVaults()
+        public
+        // alice
+        mintAlice0
+        // eth vault
+        addVault(alice0, contracts.ethVault)
+        deposit(alice0, contracts.ethVault, 100 ether)
+        // wstEth vault
+        addVault(alice0, contracts.wstEth)
+        deposit(alice0, contracts.wstEth, 100 ether)
+        // kerosene vault
+        addVault(alice0, contracts.keroseneVault)
+        deposit(alice0, contracts.keroseneVault, 5 ether)
+        mintDyad(alice0, _ethToUSD(10 ether))
+        // change assets
+        changeAsset(alice0, contracts.ethVault, 1 ether)
+        changeAsset(alice0, contracts.wstEth, 1 ether)
+        changeAsset(alice0, contracts.keroseneVault, 1 ether)
+        // bob
+        mintBob0
+        liquidate(alice0, bob0, bob)
+    {}
+
+    function testFuzz_LiquidateWithManyVaults(uint256 amount)
+        public
+        // alice
+        mintAlice0
+        // eth vault
+        addVault(alice0, contracts.ethVault)
+        deposit(alice0, contracts.ethVault, 100 ether)
+        // wstEth vault
+        addVault(alice0, contracts.wstEth)
+        deposit(alice0, contracts.wstEth, 100 ether)
+        // kerosene vault
+        addVault(alice0, contracts.keroseneVault)
+        deposit(alice0, contracts.keroseneVault, 5 ether)
+        mintDyad(alice0, _ethToUSD(10 ether))
+        // change assets
+        changeAsset(alice0, contracts.ethVault, 1 ether)
+        changeAsset(alice0, contracts.wstEth, 1 ether)
+        changeAsset(alice0, contracts.keroseneVault, 1 ether)
+        // bob
+        mintBob0
+    {
+        amount = bound(amount, 1, _ethToUSD(10 ether));
+        contracts.vaultManager.liquidate(alice0, bob0, amount);
+    }
+
+    function test_LiquidateNoCollateralLeftForLiquidatee()
+        public
+        // alice
+        mintAlice0
+        addVault(alice0, contracts.ethVault)
+        deposit(alice0, contracts.ethVault, 100 ether)
+        mintDyad(alice0, _ethToUSD(1 ether))
+        changeAsset(alice0, contracts.ethVault, 1.2 ether)
+        // bob
+        mintBob0
+        liquidate(alice0, bob0, bob)
+    {
+        uint256 ethAfter_Liquidator = contracts.ethVault.id2asset(bob0);
+        uint256 ethAfter_Liquidatee = contracts.ethVault.id2asset(alice0);
+        uint256 dyadAfter_Liquidatee = contracts.dyad.mintedDyad(alice0);
+
+        assertTrue(ethAfter_Liquidator > 0);
+        assertTrue(ethAfter_Liquidatee == 0);
+
+        assertEq(getMintedDyad(alice0), 0);
+        assertEq(getCR(alice0), type(uint256).max);
+        assertEq(dyadAfter_Liquidatee, 0);
+    }
+
+    function test_LiquidateSomeCollateralLeft()
+        public
+        // alice
+        mintAlice0
+        addVault(alice0, contracts.ethVault)
+        deposit(alice0, contracts.ethVault, 100 ether)
+        mintDyad(alice0, _ethToUSD(1 ether))
+        changeAsset(alice0, contracts.ethVault, 1.4 ether)
+        // bob
+        mintBob0
+        liquidate(alice0, bob0, bob)
+    {
+        uint256 ethAfter_Liquidator = contracts.ethVault.id2asset(bob0);
+        uint256 ethAfter_Liquidatee = contracts.ethVault.id2asset(alice0);
+        uint256 dyadAfter_Liquidatee = contracts.dyad.mintedDyad(alice0);
+
+        assertTrue(ethAfter_Liquidator > 0);
+        assertTrue(ethAfter_Liquidatee > 0);
+
+        assertEq(getMintedDyad(alice0), 0);
+        assertEq(getCR(alice0), type(uint256).max);
+        assertEq(dyadAfter_Liquidatee, 0);
+    }
+
+    function test_LiquidatePartial()
+        public
+        // alice
+        mintAlice0
+        addVault(alice0, contracts.ethVault)
+        deposit(alice0, contracts.ethVault, 100 ether)
+        addVault(alice0, contracts.wstEth)
+        deposit(alice0, contracts.wstEth, 100 ether)
+        mintDyad(alice0, _ethToUSD(50 ether))
+        changeAsset(alice0, contracts.ethVault, 50 ether)
+        changeAsset(alice0, contracts.wstEth, 10 ether)
+        // bob
+        mintBob0
+    {
+        uint256 crBefore = getCR(alice0);
+        console.log("crBefore: ", crBefore / 1e15);
+
+        uint256 debtBefore = getMintedDyad(alice0);
+        console.log("debtBefore: ", debtBefore / 1e18);
+
+        contracts.vaultManager.liquidate(alice0, bob0, _ethToUSD(10 ether));
+
+        uint256 crAfter = getCR(alice0);
+        console.log("crAfter: ", crAfter / 1e15);
+
+        uint256 debtAfter = getMintedDyad(alice0);
+        console.log("debtAfter: ", debtAfter / 1e18);
+    }
+}
