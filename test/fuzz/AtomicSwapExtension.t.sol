1d6ae171778f57ade968119683140845a7f2c4cd
Coffee☕️ <coffee@coffeebot.dev>
2024-11-05 08:01:53 -0500

    fmt

--- test/fuzz/AtomicSwapExtension.t.sol
+++ test/fuzz/AtomicSwapExtension.t.sol
@@ -6,11 +6,11 @@ import "forge-std/console.sol";
 
 contract Transit {
     function storeVariables(
-        uint256 /* noteId */,
-        address /* fromVault */,
-        uint256 /* fromAmount */,
-        address /* toVault */,
-        uint256 /* toAmount */,
+        uint256, /* noteId */
+        address, /* fromVault */
+        uint256, /* fromAmount */
+        address, /* toVault */
+        uint256, /* toAmount */
         bytes calldata /* swapData */
     ) external {
         // tstore swap data
@@ -25,93 +25,81 @@ contract Transit {
     }
 
     function loadVariables() external view returns (uint256, uint256, bytes memory) {
-      address toVault;
-      uint256 toAmount;
-      bytes memory swapData;
+        address toVault;
+        uint256 toAmount;
+        bytes memory swapData;
 
-      uint256 oldFMP;
-      uint256 newFMP;
+        uint256 oldFMP;
+        uint256 newFMP;
 
-      assembly {
-          oldFMP := mload(0x40)
-          // toVault should be the first transient slot
-          toVault := tload(0)
-          // toAmount should be the second transient slot
-          toAmount := tload(1)
-          // allocate swapData in memory
-          swapData := mload(0x40)
-          let i := 1
-          // load the size of the addresses array from the fourth transient slot
-          // we skip the third slot because it's the offset in calldata and we don't need that
-          let swapDataSize := tload(3)
-          // store the size of the swapData in the first slot of the memory array
-          mstore(swapData, swapDataSize)
-          // iterate over the addresses and copy them into the memory array
-          for {} lt(mul(sub(i, 1), 0x20), swapDataSize) { i := add(i, 1) } { // @audit mitigation Done
-              // copy from transient storage into memory
-              mstore(add(swapData, mul(i, 0x20)), tload(add(i, 3)))
-          }
-          // update the free memory pointer
-          mstore(0x40, add(swapData, add(swapDataSize, 0x20))) // @audit mitigation Done
-          newFMP := mload(0x40)
-      }
+        assembly {
+            oldFMP := mload(0x40)
+            // toVault should be the first transient slot
+            toVault := tload(0)
+            // toAmount should be the second transient slot
+            toAmount := tload(1)
+            // allocate swapData in memory
+            swapData := mload(0x40)
+            let i := 1
+            // load the size of the addresses array from the fourth transient slot
+            // we skip the third slot because it's the offset in calldata and we don't need that
+            let swapDataSize := tload(3)
+            // store the size of the swapData in the first slot of the memory array
+            mstore(swapData, swapDataSize)
+            // iterate over the addresses and copy them into the memory array
+            for {} lt(mul(sub(i, 1), 0x20), swapDataSize) { i := add(i, 1) } {
+                // @audit mitigation Done
+                // copy from transient storage into memory
+                mstore(add(swapData, mul(i, 0x20)), tload(add(i, 3)))
+            }
+            // update the free memory pointer
+            mstore(0x40, add(swapData, add(swapDataSize, 0x20))) // @audit mitigation Done
+            newFMP := mload(0x40)
+        }
 
-      return (oldFMP, newFMP, swapData);
-
+        return (oldFMP, newFMP, swapData);
     }
 }
 
 contract EntryPoint {
+    address public transit;
 
-  address public transit;
+    constructor(address _transit) {
+        transit = _transit;
+    }
 
-  constructor(address _transit) {
-    transit = _transit;
-  }
+    function testAtomicSwap(bytes calldata swapData) external returns (uint256, bytes memory) {
+        Transit(transit).storeVariables(0x5, address(0xff), 0x1000, address(0xbb), 0x5000, swapData);
 
-  function testAtomicSwap(bytes calldata swapData) external returns(uint256, bytes memory) {
-    Transit(transit).storeVariables(
-      0x5,
-      address(0xff),
-      0x1000,
-      address(0xbb),
-      0x5000,
-      swapData
-    );
+        (uint256 oldFMP, uint256 newFMP, bytes memory returnedSwapData) = Transit(transit).loadVariables();
 
-    (uint256 oldFMP, uint256 newFMP, bytes memory returnedSwapData) = Transit(transit).loadVariables();
-    
-    require(newFMP > oldFMP, "newFMP is smalelr than oldFMP");
+        require(newFMP > oldFMP, "newFMP is smalelr than oldFMP");
 
-    return (newFMP - oldFMP, returnedSwapData);
-  }
-
-
+        return (newFMP - oldFMP, returnedSwapData);
+    }
 }
 
 contract AtomicSwapExtensionTest is Test {
+    EntryPoint entryPoint;
 
-  EntryPoint entryPoint;
-
-
-  function setUp() public {
-    address __transit = address(new Transit());
-    entryPoint = new EntryPoint(__transit);
-  }
-
-  function testMemoryManagmentFuzzing(uint256 number) public {
-    uint256[] memory array = new uint256[]((number % 100) + 1);
-
-    for (uint i = 0; i < array.length; i++) {
-      array[i] = type(uint256).max;
+    function setUp() public {
+        address __transit = address(new Transit());
+        entryPoint = new EntryPoint(__transit);
     }
 
-    bytes4 functionSignature = bytes4(0x11223344);
+    function testMemoryManagmentFuzzing(uint256 number) public {
+        uint256[] memory array = new uint256[]((number % 100) + 1);
 
-    bytes memory swapData = abi.encodePacked(functionSignature, array);
-    uint256 callEncodeLength = swapData.length /* func signature + data */ + 0x20; /* The length offset */
-    (uint256 memoryAllocated, bytes memory returnedSwapData) = entryPoint.testAtomicSwap(swapData);
-    assertEq(callEncodeLength, memoryAllocated);
-    assertEq(keccak256(returnedSwapData), keccak256(swapData));
-  }
+        for (uint256 i = 0; i < array.length; i++) {
+            array[i] = type(uint256).max;
+        }
+
+        bytes4 functionSignature = bytes4(0x11223344);
+
+        bytes memory swapData = abi.encodePacked(functionSignature, array);
+        uint256 callEncodeLength = swapData.length /* func signature + data */ + 0x20; /* The length offset */
+        (uint256 memoryAllocated, bytes memory returnedSwapData) = entryPoint.testAtomicSwap(swapData);
+        assertEq(callEncodeLength, memoryAllocated);
+        assertEq(keccak256(returnedSwapData), keccak256(swapData));
+    }
 }
