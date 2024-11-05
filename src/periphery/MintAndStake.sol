3b24576923623e6b969907a469fa02b1ed5c05af
shafu <shafu0x@gmail.com>
2024-11-05 13:02:58 +0100

    MintAndStake

---
+++ src/periphery/MintAndStake.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IExtension} from "../interfaces/IExtension.sol";
+import {ICurvePool} from "../interfaces/ICurvePool.sol";
+import {IDyadLPStaking} from "../interfaces/IDyadLPStaking.sol";
+import {DNft} from "../core/DNft.sol";
+import {VaultManagerV5} from "../core/VaultManagerV5.sol";
+import {Dyad} from "../core/Dyad.sol";
+
+contract MintAndStake is IExtension {
+  DNft public immutable dNft;
+  VaultManagerV5 public immutable vaultManager;
+  Dyad public immutable dyad;
+
+  constructor(
+    address _dNft,
+    address _vaultManager,
+    address _dyad
+  ) {
+    dNft = DNft(_dNft);
+    vaultManager = VaultManagerV5(_vaultManager);
+    dyad = Dyad(_dyad);
+  }
+
+  function name() external pure override returns (string memory) {
+    return "Mint and Stake";
+  }
+
+  function description() external pure override returns (string memory) {
+    return "Mint DYAD, provide Liquidity, and stake LP tokens automatically";
+  }
+
+  function getHookFlags() external pure override returns (uint256) {
+    return 0; // no hooks needed for this extension
+  }
+
+  function mintAndStake(
+      uint tokenId,
+      uint amount,
+      address pool,
+      uint dyadIndex,
+      address stakingContract,
+      uint minAmountOut
+  ) external {
+      require(dNft.ownerOf(tokenId) == msg.sender, "NOT_DNFT_OWNER");
+      vaultManager.mintDyad(tokenId, amount, address(this));
+      dyad.approve(pool, amount);
+      uint256[] memory amounts = new uint256[](ICurvePool(pool).N_COINS());
+      amounts[dyadIndex] = amount;
+      uint lpAmount = ICurvePool(pool).add_liquidity(amounts, minAmountOut, address(this));
+      dyad.approve(stakingContract, lpAmount);
+      IDyadLPStaking(stakingContract).deposit(tokenId, lpAmount);
+  }
+}
